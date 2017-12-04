bac8034ea8d3c6b1602210ca110f395854365b18
seal <chenkerenqiqi@163.com>
2017-12-04 15:05:33 +0800

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,66 @@
+/**
+ * Created by seal on 12/4/17.
+ */
+const DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+const DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+const DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
+const HIDDEN_DETAIL_CLASS = 'hidden-detail';
+const TINY_EFFECT_CLASS = 'is-tiny'
+const ESC_KEY = 27;
+
+function setDetails(imgUrl, titleText) {
+    let detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+    let detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+    detailImage.setAttribute('src', imgUrl);
+    detailTitle.textContent = titleText;
+}
+function imageFromThumb(thumbnail) {
+    return thumbnail.getAttribute('data-image-url')
+}
+function titleFromThumb(thumbnail) {
+    return thumbnail.getAttribute('data-image-title')
+}
+function setDetailsFromThumb(thumbnail) {
+    setDetails(imageFromThumb(thumbnail),titleFromThumb(thumbnail))
+}
+function addThumbClickHandler(thumb) {
+    thumb.addEventListener('click', function (event) {
+        event.preventDefault();
+        setDetailsFromThumb(thumb);
+        showDetail();
+    })
+}
+function getThumbnailsArray() {
+    let thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+    let thumbnailArray = [].slice.call(thumbnails)
+    return thumbnailArray;
+}
+function hideDetails() {
+    document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+function showDetail() {
+    let frame = document.querySelector(DETAIL_FRAME_SELECTOR)
+    document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+    frame.classList.add(TINY_EFFECT_CLASS);
+    setTimeout(function () {
+        frame.classList.remove(TINY_EFFECT_CLASS);
+    }, 50);
+
+}
+function addKeyPressHandler() {
+    document.body.addEventListener('keyup', function (event) {
+        event.preventDefault();
+        console.log(event.keyCode)
+        if (event.keyCode === ESC_KEY) {
+            hideDetails();
+        }
+    })
+}
+
+function initializeEvent() {
+    let thumbnails = getThumbnailsArray();
+    thumbnails.forEach(addThumbClickHandler);
+    addKeyPressHandler();
+}
+initializeEvent();
